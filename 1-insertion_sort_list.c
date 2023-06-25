205f518f59bc36eb9986856f903327a007d60a59
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-06-25 23:10:21 +0300

    Task #1 Insertion

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list
+ * in acsending order using Insertion Sort algorithm
+ * @list: The Doubly Linked List
+ */
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *current_node = *list, *tmp;
+
+    if (list == NULL || *list == NULL)
+        return;
+
+    while (current_node != NULL)
+    {
+        tmp = current_node;
+        while (tmp->prev != NULL && tmp->n < tmp->prev->n)
+        {
+            tmp->prev->next = tmp->next;
+            if (tmp->next != NULL)
+                tmp->next->prev = tmp->prev;
+            tmp->next = tmp->prev;
+            tmp->prev = tmp->prev->prev;
+            tmp->next->prev = tmp;
+            if (tmp->prev == NULL)
+                *list = tmp;
+            else
+                tmp->prev->next = tmp;
+            print_list(*list);
+        }
+        current_node = current_node->next;
+    }
+}
